dcf3b75dfc08ec8babe3a09c8a743be4081e2d00
Sasha <73294558+2JustFox2@users.noreply.github.com>
2025-11-29 20:07:05 +0300

    feat: add inversify for dependency injection
    - Added inversify and its dependencies to package.json and package-lock.json
    - Updated src/app.ts to use inversify for dependency injection
    - Added reflect-metadata as a peer dependency for inversify
    - Updated package-lock.json with new dependencies and their versions
    - Updated package.json with new dependencies and their versions

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 import express, { Express } from "express";
 import { Server } from "http";
+import { inject } from "inversify";
+import { TYPES } from "./types";
+import { ILogger } from "./logger/logger.interface";
 
 export class App {
     app: Express;
     port: number;
     server: Server;
 
-    constructor() {
+    constructor(
+        @inject(TYPES.ILogger) private logger: ILogger
+    ) {
         this.app = express();
         this.port = 8000
     }
 
     public async init() {
         this.server = this.app.listen(this.port);
-        console.log(`Server is runner on "http://localhost:${this.port}"`)
+        this.logger.log(`Server is runner on "http://localhost:${this.port}"`)
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
+import { Container, ContainerModule, ContainerModuleLoadOptions } from "inversify"
 import { App } from "./app"
+import { ILogger } from "./logger/logger.interface"
+import { TYPES } from "./types"
+import { LoggerService } from "./logger/logger.service"
+
+export const appBindings = new ContainerModule(
+    (options: ContainerModuleLoadOptions) => {
+        options.bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope()
+        options.bind<App>(TYPES.Application).to(App)
+    }
+)
 
 async function bootstrap() {
-    const app = new App()
+    const appContainer = new Container();
+    appContainer.load(appBindings)
+    const app = appContainer.get<App>(TYPES.Application)
     app.init()
     return { app }
 }
