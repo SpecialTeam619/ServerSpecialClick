134a58d01e6324e3fdca5939a76349f5d8f1fe1f
Sasha <73294558+2JustFox2@users.noreply.github.com>
2026-01-07 17:26:00 +0300

    feat: add dotenv and update prisma client provider

---
+++ src/prisma/migrations/20260107142513_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('CUSTOMER', 'LESSOR');
+
+-- CreateTable
+CREATE TABLE "UserModel" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'CUSTOMER',
+
+    CONSTRAINT "UserModel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserModel_email_key" ON "UserModel"("email");
